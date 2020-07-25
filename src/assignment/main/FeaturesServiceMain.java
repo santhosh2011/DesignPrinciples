6350f9314d65c12c65756549e79cb30f1fb9e498
Santhosh Thota <sathota@fb.com>
2020-07-25 22:49:00 +0530

    Adding new default files for the Feature service and to test the service

---
+++ src/assignment/main/FeaturesServiceMain.java
@@ -0,0 +1,8 @@
+package assignment.main;
+
+public class FeaturesServiceMain {
+    public static void main(String[] args) {
+        // Code to test the service
+
+    }
+}
---
+++ src/assignment/service/FeatureService.java
@@ -0,0 +1,11 @@
+package assignment.service;
+
+import java.util.HashMap;
+
+public class FeatureService {
+
+    static boolean isallowed(String feature, String condition, HashMap<String, Object> user) {
+        //TODO: need to implement the code
+        return true;
+    }
+}
