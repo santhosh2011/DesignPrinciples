5bca5f2d72082a35ff0bfb9c4a7a650bbf1efa49
Santhosh Thota <sathota@fb.com>
2020-07-25 22:49:46 +0530

    Adding IUser interface and its adapter

---
+++ src/assignment/models/IUser.java
@@ -0,0 +1,6 @@
+package assignment.models;
+
+public interface IUser {
+    //TODO: Base user Interface for the Service
+
+}
---
+++ src/assignment/models/MapUserAdapter.java
@@ -0,0 +1,9 @@
+package assignment.models;
+
+import java.util.HashMap;
+
+public class MapUserAdapter implements IUser {
+    private HashMap<String, Object> user;
+
+    //TODO: implement base user properties
+}
